0d35a0a20b60966ff830928bb5a6886d500ef5d3
Jose <netopereira464@gmail.com>
2023-06-06 09:40:54 -0300

    My first commit in project profile settings

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <h1>My profile-settings</h1>
+    </div>
+  )
+}
+
+export default App
